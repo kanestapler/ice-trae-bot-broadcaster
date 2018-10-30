06a7d2a74fef3779f1193c501e043784e5080f91
kanestapler <kanestapler@gmail.com>
2018-10-30 00:27:37 -0700

    Tweet API for game start and any text

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 const PORT = 3002
 const TWEET_EMOJI = '❄️'
+const GAME_START_TEXT = 'Ice Trae Ready #BeatTheTEAM_NAME'
 const TWITTER_POST_URL = 'https://api.twitter.com/1.1/statuses/update.json'
 
 var oauth = new OAuth.OAuth(
@@ -20,7 +21,7 @@ var oauth = new OAuth.OAuth(
 )
 app.get('/', (req, res) => res.send('Tweet Bot Running'))
 
-app.post('/', function (req, res) {
+app.post('/shotMade', function (req, res) {
     const amount = req.body.amount
     const token = req.body.token
     if (token !== process.env.API_TOKEN) {
@@ -28,7 +29,7 @@ app.post('/', function (req, res) {
         res.send({ message: 'incorrect token' })
     } else {
         console.log(`Tweeting ${amount} snowflakes`)
-        postTweet(amount).then(function (data) {
+        tweetMadeShot(amount).then(function (data) {
             console.log('Successful Tweet')
             res.send(`POST Tweet with ${amount} snowflakes`)
         }).catch(function (error) {
@@ -38,12 +39,58 @@ app.post('/', function (req, res) {
     }
 })
 
+app.post('/gameStart', function (req, res) {
+    const opponent = req.body.opponent
+    const token = req.body.token
+    if (token !== process.env.API_TOKEN) {
+        res.status(401)
+        res.send({ message: 'incorrect token' })
+    } else {
+        console.log(`Tweeting ${opponent} game start`)
+        tweetGameStart(opponent).then(function (data) {
+            console.log('Successful Tweet')
+            res.send(`POST Tweet with ${opponent} game starting`)
+        }).catch(function (error) {
+            console.log('ERROR Tweeting')
+            res.send(error)
+        })
+    }
+})
+
+app.post('/text', function (req, res) {
+    const text = req.body.text
+    const token = req.body.token
+    if (token !== process.env.API_TOKEN) {
+        res.status(401)
+        res.send({ message: 'incorrect token' })
+    } else {
+        console.log(`Tweeting ${text}`)
+        postTweet(text).then(function (data) {
+            console.log('Successful Tweet')
+            res.send(`POST Tweet with ${text}`)
+        }).catch(function (error) {
+            console.log('ERROR Tweeting')
+            res.send(error)
+        })
+    }
+})
+
 app.listen(PORT, () => console.log(`Tweet Bot app listening on port ${PORT}!`))
 
-function postTweet(amount) {
+function tweetGameStart(opponent) {
+    const tweetBody = GAME_START_TEXT.replace('TEAM_NAME', opponent)
+    return postTweet(tweetBody)
+}
+
+function tweetMadeShot(amount) {
     amount = Number(amount)
     const tweetBody = TWEET_EMOJI.repeat(amount)
     console.log(`tweet body: ${tweetBody}`)
+    return postTweet(tweetBody)
+}
+
+function postTweet(tweetBody) {
+    console.log(tweetBody)
     return new Promise(function (resolve, reject) {
         oauth.post(
             TWITTER_POST_URL,
