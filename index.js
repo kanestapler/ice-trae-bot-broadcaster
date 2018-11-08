141d2062de875e3ce68856ac803e7414f8a35985
kanestapler <kanestapler@gmail.com>
2018-11-07 23:44:15 -0800

    Adding in linter

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser')
 const serverless = require('serverless-http')
 const rp = require('request-promise')
 require('dotenv').config()
+
 const app = express()
 app.use(bodyParser.json())
 const PORT = 3002
 const TWITTER_POST_URL = 'https://api.twitter.com/1.1/statuses/update.json'
 const TWEET_URL = 'https://twitter.com/USER_NAME/status/TWEET_ID'
 
-var oauth = new OAuth.OAuth(
+const oauth = new OAuth.OAuth(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/access_token',
     process.env.CONSUMER_KEY,
@@ -21,74 +22,80 @@ var oauth = new OAuth.OAuth(
 )
 app.get('/', (req, res) => res.send('Tweet Bot Running'))
 
-app.post('/emojis', function (req, res) {
-    const amount = req.body.amount
-    const token = req.body.token
-    const slack = req.body.slack
+app.post('/emojis', (req, res) => {
+    const {
+        amount,
+        token,
+        slack,
+    } = req.body
     if (token !== process.env.API_TOKEN) {
         res.status(401)
         res.send({ message: 'incorrect token' })
     } else {
         console.log(`Tweeting ${amount} snowflakes`)
-        tweetMadeShot(amount).then(function (twitterResponse) {
+        tweetMadeShot(amount).then((twitterResponse) => {
             console.log('Successful Tweet')
             if (slack) {
                 const tweetUrl = getTwitterURL(twitterResponse)
-                postToSlack(tweetUrl).then(function (success) {
+                postToSlack(tweetUrl).then(() => {
                     res.send(`POST Tweet and Slack with ${amount} snowflakes`)
-                }).catch(function (responseError) {
+                }).catch((responseError) => {
                     res.send(`Error during Post to Slack. Tweet posted Successfully. ${responseError}`)
                 })
             } else {
                 res.send(`POST Tweet with ${amount} snowflakes`)
             }
-        }).catch(function (error) {
-            console.log('ERROR Tweeting')
-            res.send(`Error posting tweet`)
+        }).catch((error) => {
+            console.log('ERROR Tweeting', error)
+            res.send('Error posting tweet')
         })
     }
 })
 
-app.post('/gameStart', function (req, res) {
-    const opponent = req.body.opponent
-    const token = req.body.token
-    const slack = req.body.slack
+app.post('/gameStart', (req, res) => {
+    const {
+        opponent,
+        token,
+        slack,
+    } = req.body
     if (token !== process.env.API_TOKEN) {
         res.status(401)
         res.send({ message: 'incorrect token' })
     } else {
         console.log(`Tweeting ${opponent} game start`)
-        tweetGameStart(opponent).then(function (twitterResponse) {
+        tweetGameStart(opponent).then((twitterResponse) => {
             console.log('Successful Tweet')
             if (slack) {
                 const tweetUrl = getTwitterURL(twitterResponse)
-                postToSlack(tweetUrl).then(function (success) {
+                postToSlack(tweetUrl).then(() => {
                     res.send(`POST Tweet and Slack with ${opponent} game starting`)
-                }).catch(function (responseError) {
+                }).catch((responseError) => {
                     res.send(`Error during Post to Slack. Tweet posted Successfully. ${responseError}`)
                 })
             } else {
                 res.send(`POST Tweet with ${opponent} game starting`)
             }
-        }).catch(function (error) {
+        }).catch((error) => {
             console.log('ERROR Tweeting')
             res.send(error)
         })
     }
 })
 
-app.post('/text', function (req, res) {
-    const text = req.body.text
-    const token = req.body.token
+app.post('/text', (req, res) => {
+    const {
+        text,
+        token,
+    } = req.body
     if (token !== process.env.API_TOKEN) {
         res.status(401)
         res.send({ message: 'incorrect token' })
     } else {
         console.log(`Tweeting ${text}`)
-        postTweet(text).then(function (data) {
+        postTweet(text).then(() => {
             console.log('Successful Tweet')
             res.send(`POST Tweet with ${text}`)
-        }).catch(function (error) {
+        }).catch((error) => {
             console.log('ERROR Tweeting')
             res.send(error)
         })
@@ -103,21 +110,21 @@ function tweetGameStart(opponent) {
 }
 
 function tweetMadeShot(amount) {
-    amount = Number(amount)
-    const tweetBody = process.env.TWEET_EMOJI.repeat(amount)
+    const amountNumber = Number(amount)
+    const tweetBody = process.env.TWEET_EMOJI.repeat(amountNumber)
     console.log(`tweet body: ${tweetBody}`)
     return postTweet(tweetBody)
 }
 
 function postTweet(tweetBody) {
     console.log(tweetBody)
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
         oauth.post(
             TWITTER_POST_URL,
             process.env.ACCESS_TOKEN,
             process.env.ACCESS_TOKEN_SECRET,
-            { "status": tweetBody },
-            function (error, data) {
+            { status: tweetBody },
+            (error, data) => {
                 if (error) {
                     reject(error)
                 } else {
@@ -139,10 +146,10 @@ function postToSlack(body) {
         method: 'POST',
         uri: url,
         body: {
-            text: body
+            text: body,
         },
-        json: true
-    };
+        json: true,
+    }
     return rp.post(options)
 }
 
